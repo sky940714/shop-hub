211fa4418851cb1ca012e58d6e519f268fabec0d
kc markel <kcmarkel@kcdeMacBook-Air.local>
2025-11-27 15:01:10 +0800

    修正：在登入 API 回傳 user 物件中加入 role 欄位

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
 import HomePage from './pages/HomePage';
 import MemberPage from './pages/MemberPage';
 import WishlistPage from './pages/WishlistPage';
@@ -9,57 +10,58 @@ import LoginPage from './pages/LoginPage';
 import AdminLayout from './pages/admin/AdminLayout';
 import ProtectedRoute from './components/ProtectedRoute';
 import ProductDetailPage from './pages/ProductDetailPage';
-import { CartProvider } from './context/CartContext'; 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* 首頁 - 公開訪問 */}
-        <Route path="/" element={<HomePage />} />
+    <CartProvider>
+      <Router>
+        <Routes>
+          {/* 首頁 - 公開訪問 */}
+          <Route path="/" element={<HomePage />} />
 
-        <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
 
-        {/* 登入頁面 */}
-        <Route path="/login" element={<LoginPage />} />
+          {/* 登入頁面 */}
+          <Route path="/login" element={<LoginPage />} />
 
-        {/* 前台路由 - 需要登入 */}
-        <Route
-          path="/member"
-          element={
-            <ProtectedRoute>
-              <MemberPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/wishlist"
-          element={
-            <ProtectedRoute>
-              <WishlistPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/search"
-          element={
-            <ProtectedRoute>
-              <SearchPage />
-            </ProtectedRoute>
-          }
-        />
+          {/* 前台路由 - 需要登入 */}
+          <Route
+            path="/member"
+            element={
+              <ProtectedRoute>
+                <MemberPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/wishlist"
+            element={
+              <ProtectedRoute>
+                <WishlistPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/search"
+            element={
+              <ProtectedRoute>
+                <SearchPage />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* 後台路由 - 需要管理員權限 */}
-        <Route
-          path="/admin/*"
-          element={
-            <ProtectedRoute requireAdmin={true}>
-              <AdminLayout />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
+          {/* 後台路由 - 需要管理員權限 */}
+          <Route
+            path="/admin/*"
+            element={
+              <ProtectedRoute requireAdmin={true}>
+                <AdminLayout />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
